f5549edf0ab01e78cc4a7a768db4a4b5a20fab71
Gergely Sipos <gsipos.ext@nemetschek.com>
2025-10-30 20:30:24 +0100

    fix formatting

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -7,7 +7,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
-      theme={theme as ToasterProps['theme'] ?? 'system'}
+      theme={(theme as ToasterProps['theme']) ?? 'system'}
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
