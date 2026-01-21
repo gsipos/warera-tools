6ba72e9e0120e14be74049ca33a6ab39441668ed
Gergely Sipos <gsipos.ext@nemetschek.com>
2026-01-21 23:58:50 +0100

    fix compiulation

--- src/hooks/game/use-top-work-offer-wage.ts
+++ src/hooks/game/use-top-work-offer-wage.ts
@@ -1,6 +1,8 @@
 import { useWorkOffers } from '@/api/warera-api'
+import { flattenPaginatedQuery } from '@/functions/flatten-paginated-data'
 
 export const useTopWorkOfferWage = () => {
   const workOffers = useWorkOffers(1)
-  return workOffers.data?.items?.[0]?.wage ?? 0
+  const data = flattenPaginatedQuery(workOffers)
+  return data?.[0]?.wage ?? 0
 }
